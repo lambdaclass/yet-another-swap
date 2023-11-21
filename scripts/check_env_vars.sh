aa5425d1daa3ee1adfdb3b7bf6015920208292fd
dpinones <pinonesdamian@gmail.com>
2023-11-21 16:21:01 -0300

    change directory account katana

--- scripts/check_env_vars.sh
+++ scripts/check_env_vars.sh
@@ -5,7 +5,7 @@ if [ -z "${STARKNET_KEYSTORE}" ]; then
     echo ""
     echo "Did you declare the STARKNET_KEYSTORE environment variable?"
     echo "It should look something like this:"
-    echo '   export STARKNET_KEYSTORE="~/.starkli-wallets/keystore.json"'
+    echo '   export STARKNET_KEYSTORE="~/.starkli/keystore.json"'
     echo "Declare it and try again! :)"
     echo ""
     exit 1
@@ -15,7 +15,7 @@ if [ -z "${STARKNET_ACCOUNT}" ]; then
     echo "Error: STARKNET_ACCOUNT environment variable is not defined."
     echo "Did you declare the STARKNET_ACCOUNT environment variable?"
     echo "It should look something like this:"
-    echo '   export STARKNET_ACCOUNT="~/.starkli-wallets/account.json"'
+    echo '   export STARKNET_ACCOUNT="~/.starkli/account.json"'
     echo "Declare it and try again! :)"
     echo ""
     exit 2
